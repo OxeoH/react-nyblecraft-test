fc28fca05a347d06cc519e15e705a52e07398cb9
OxeoH <egor33429@gmail.com>
2023-03-21 18:52:36 +0300

    redesign, fixed unique tags feature

--- src/utils/findTagsInStroke.ts
+++ src/utils/findTagsInStroke.ts
@@ -1,11 +1,7 @@
 export const template = /#\b[a-z-0-9]+\b/gi
 
-// \b - граница слова
-// /a+/ соответствует 'a' в "candy" и всем символам 'a' в "caaaaaaandy"
-// /gi глобальный поиск и игнор регистра
-
 export const findTagsInStroke = (noteText: string) => {
   const result = noteText.match(template)
 
-  return result ? result : []
+  return result ? Array.from(new Set(result)) : []
 }
