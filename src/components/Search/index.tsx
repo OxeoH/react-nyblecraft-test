a4aa98ec003e0c6d846c18f9e08ab2fa353c6b76
OxeoH <egor33429@gmail.com>
2023-03-22 00:28:24 +0300

    added useDebounce hook to search widget

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,30 +1,35 @@
 import { observer } from 'mobx-react-lite'
 import React, { ChangeEvent } from 'react'
 import { useStore } from '../../store'
+import useDebounce from '../../utils/hooks/useDebounce'
 import { MyInput } from '../UI/MyInput'
 
 import styles from './Search.module.scss'
 
 export const Search: React.FC = observer(() => {
   const { notesStore } = useStore()
+  const [value, setValue] = React.useState(notesStore.searchValue);
 
   const clearSearch = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     notesStore.setSearchValue('')
+    setValue('')
   }
 
-  const onSearchChange = (value: string) => {
+  const debouncedValue = useDebounce<string>(value, 500)
+
+  React.useEffect(() => {
     notesStore.setSearchValue(value)
-  }
+  }, [debouncedValue])
 
   return (
     <div className={styles.wrapper}>
       <label className={styles.label}>Search:</label>
       <input
         type="text"
-        value={notesStore.searchValue}
+        value={value}
         placeholder="Search by tag..."
         className={styles.search}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => onSearchChange(e.target.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
       />
       <div className={styles.cross} onClick={e => clearSearch(e)} />
     </div>
